ec3032ac80d9044604e7d7785e395fa6b692c1f0
feridarmawan15 <feridarmawan15@gmail.com>
2026-05-30 16:48:56 +0700

    Add dummy data insertion

--- database.js
+++ database.js
@@ -15,6 +15,18 @@ let db = new sqlite3.Database(dbName, (err) => {
         )`, (err) => {
             if (err) {
                 console.error(err.message);
+            } else {
+                // Insert dummy data if the table is empty
+                db.get("SELECT COUNT(*) AS count FROM products", (err, row) => {
+                    if (row && row.count === 0) {
+                        const insert = 'INSERT INTO products (name, price, stock, category) VALUES (?,?,?,?)';
+                        db.run(insert, ["Mie Instan Goreng", 3500, 50, "Makanan"]);
+                        db.run(insert, ["Kopi Susu Botol", 8000, 30, "Minuman"]);
+                        db.run(insert, ["Buku Tulis Sinar Dunia", 4500, 100, "Alat Tulis"]);
+                        db.run(insert, ["Pensil 2B", 3000, 150, "Alat Tulis"]);
+                        console.log('Dummy data inserted.');
+                    }
+                });
             }
         });
     }
